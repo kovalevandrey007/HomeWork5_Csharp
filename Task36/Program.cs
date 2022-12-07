383000a251707b72d55318e8f93e646937ff48b3
Андрей Ковалев <kovalevandrey007@gmail.com>
2022-12-07 19:39:44 +0200

    doneTask35

---
+++ Task36/Program.cs
@@ -0,0 +1,83 @@
+﻿/*Задача 36: Задайте одномерный массив, 
+заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+
+int[] InitArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(-9,10);
+    }
+
+    return array;
+}
+
+//печатает массив на экран
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//получает сумму положительных элементов в массиве
+int GetPositiveSumm(int[] arr)
+{
+    int summ = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+            summ += arr[i];
+    }
+
+    return summ;
+}
+
+//кортедж
+//получает сумму отрицательных элементов в массиве
+int GetNegativeSumm(int[] arr)
+{
+    int summ = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < 0)
+            summ += arr[i];
+    }
+
+    return summ;
+}
+
+(int, int) GetPositiveAndNegativeSumm(int[] array)
+{
+    int positive = 0;
+    int negative = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+            positive += array[i];
+        else
+            negative += array[i];
+    }
+
+    return (positive, negative);
+}
+
+int[] arr = InitArray(12);
+PrintArray(arr);
+int positiveSumm = GetPositiveSumm(arr);
+int negativeSumm = GetNegativeSumm(arr);
+
+(int pos, int neg) = GetPositiveAndNegativeSumm(arr);
+
+Console.WriteLine($"Сумма положительных: {positiveSumm}. Сумма отрицательных: {negativeSumm}");
+
+Console.WriteLine($"Сумма положительных: {pos}. Сумма отрицательных: {neg}");
