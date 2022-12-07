9196f7f7a0a649c02cbce024d8799b085a776305
Андрей Ковалев <kovalevandrey007@gmail.com>
2022-12-07 19:49:45 +0200

    doneTask35

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -16,7 +16,7 @@
 int[] GetArray(int length)
 {
     Random rnd = new Random();
-    int [] array = new int [123];
+    int [] array = new int [5];
     for (int i = 0; i < length; i++)
     {
         array[i] = rnd.Next(length);
@@ -27,25 +27,30 @@ int[] GetArray(int length)
 int Count(int[] array)
 {
     int count = 0;
-    for (int i = 0; i < 123; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] >=10 && array[i]<= 99) count++;
+        if (array[i] >=100 && array[i]<= 999) count++;
     }
     return count;
 }
 
 void PrintArray(int [] array)
 {
-    for (int i = 0; i < 123; i++)
+    for (int i = 0; i < array.Length; i++)
+    if(array[i]%2 == 0)
     {
         Console.Write($"{array[i]}, ");
+    }
+    else
+    {
+        Console.Write("нет ");
     }
 }
 
 
-Console.WriteLine("Ведите размер массива");
+Console.WriteLine("Чтобы задать массив нажмите ENTER");
 int[] array = GetArray(int.Parse(Console.ReadLine()));
 
 PrintArray(array);
 Console.WriteLine();
-Console.WriteLine($"Количество элементов массива, лежащие на отрезке [10,99] = {Count(array)}");
+Console.WriteLine($"Количество чётных чисел в массиве = {Count(array)}");
